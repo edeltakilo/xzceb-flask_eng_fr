d165787d19177deda63e22a3e83ead8d32b4814a
edeltakilo <edeltakilo@gmail.com>
2022-04-16 15:17:02 +0000

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,33 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_transator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_transator.set_service_url(url)
+
+def english_to_french(english_text):
+    translation = language_transator.translate(
+        text=english_text,
+        model_id='en-fr'
+    ).get_result()
+    return translation['translations'][0]['translation']
+
+def french_to_english(french_text):
+    translation = language_transator.translate(
+        text=french_text,
+        model_id='fr-en'
+    ).get_result()
+    return translation['translations'][0]['translation']
+    
